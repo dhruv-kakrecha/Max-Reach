caa6968d729f945195e0fc7f4398ceafe0eb0c66
dhruv-kakrecha <160730440+dhruv-kakrecha@users.noreply.github.com>
2025-01-24 16:20:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-
-// app.use(cors());
-
-// app.listen(8000, () => {
-//     console.log("server is start now http://localhost:8000");
-// })
-
-// app.use("/", require("./routes/appRoutes"));
-
-// app.get('/', (req, res) => {
-
-//     const { name } = req.query
-//     res.json({ status: true, message: `welcome ${name || "dhruv"}` });
-// })
-
-
-//server.js
-
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.listen(8000, () => {
-    console.log("Server Started at port no. 8000");
+    console.log("server is start now http://localhost:8000");
 })
 
 app.use("/", require("./routes/appRoutes"));
-app.use("/user", require("./routes/userRoutes"));
 
-//Mongoose library instance
-const mongoose = require('mongoose');
-//URL of MongoDB Database
-const mongoDBURL = 'mongodb://127.0.0.1:27017';
+app.get('/', (req, res) => {
 
-//connect to Database
-mongoose.connect(mongoDBURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    const { name } = req.query
+    res.json({ status: true, message: `welcome ${name || "dhruv"}` });
 })
-    .then(() => { console.log("Connection Successfull") })
-    .catch((err) => { console.log("Received an Error") })
